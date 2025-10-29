e5bf3099a11931a0cbfe33939bdd97cc5329b5fb
Erika <2530460+xne@users.noreply.github.com>
2025-10-29 12:55:17 -0400

    Add FPS character

---
+++ Source/VGP221_A1/Private/FPSCharacter.cpp
@@ -0,0 +1,21 @@
+#include "FPSCharacter.h"
+
+AFPSCharacter::AFPSCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AFPSCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AFPSCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AFPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
---
+++ Source/VGP221_A1/Public/FPSCharacter.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "FPSCharacter.generated.h"
+
+UCLASS()
+class VGP221_A1_API AFPSCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AFPSCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
