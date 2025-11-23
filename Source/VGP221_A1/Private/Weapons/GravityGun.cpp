3ea9c398753c774caf5cf04a1dc967de8dc9e5d8
Erika <2530460+xne@users.noreply.github.com>
2025-11-23 02:34:58 -0500

    Reimplement gravity gun in C++

---
+++ Source/VGP221_A1/Private/Weapons/GravityGun.cpp
@@ -0,0 +1,77 @@
+#include "Weapons/GravityGun.h"
+
+AGravityGun::AGravityGun()
+{
+	if (!PhysicsHandleComponent)
+	{
+		PhysicsHandleComponent = CreateDefaultSubobject<UPhysicsHandleComponent>(TEXT("PhysicsHandleComponent"));
+	}
+}
+
+void AGravityGun::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (!bGrabActive)
+		return;
+
+	FVector ComponentLocation = GrabbedComponent->GetComponentLocation();
+	float Margin = GrabbedComponent->GetLocalBounds().SphereRadius + 100.f;
+
+	if (FVector::Dist(ComponentLocation, GrabLocation) > Margin)
+	{
+		Release();
+		return;
+	}
+
+	GrabLocation = GetActorLocation() + GetActorForwardVector() * GrabDistance;
+	PhysicsHandleComponent->SetTargetLocationAndRotation(GrabLocation, GetActorRotation());
+}
+
+void AGravityGun::OnFire_Implementation()
+{
+	if (bGrabActive)
+		Release();
+	else
+		Grab();
+}
+
+void AGravityGun::OnZoom_Implementation(float Value)
+{
+	if (!bGrabActive)
+		return;
+	
+	GrabDistance = FMath::Clamp(GrabDistance + Value * ZoomSpeed, GrabMinDistance, GrabMaxDistance);
+}
+
+bool AGravityGun::Grab()
+{
+	FVector Direction = GetActorRotation().Vector();
+
+	FVector Start = GetActorLocation();
+	FVector End = Start + (Direction * GrabMaxDistance);
+	FCollisionQueryParams CollisionParams;
+	CollisionParams.AddIgnoredActor(this);
+	FHitResult Result;
+	auto bHit = GetWorld()->LineTraceSingleByChannel(Result, Start, End, ECC_Visibility, CollisionParams);
+
+	if (!bHit)
+		return false;
+
+	GrabDistance = Result.Distance;
+	GrabLocation = Result.ImpactPoint;
+	GrabbedComponent = Result.GetComponent();
+
+	if (!GrabbedComponent->IsSimulatingPhysics())
+		return false;
+
+	PhysicsHandleComponent->GrabComponentAtLocationWithRotation(GrabbedComponent, NAME_None, GrabLocation, GetActorRotation());
+	bGrabActive = true;
+	return true;
+}
+
+void AGravityGun::Release()
+{
+	PhysicsHandleComponent->ReleaseComponent();
+	bGrabActive = false;
+}
---
+++ Source/VGP221_A1/Public/Weapons/GravityGun.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/PrimitiveComponent.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
+#include "Weapons/FPSWeapon.h"
+#include "GravityGun.generated.h"
+
+UCLASS()
+class VGP221_A1_API AGravityGun : public AFPSWeapon
+{
+	GENERATED_BODY()
+
+public:
+	AGravityGun();
+
+protected:
+	virtual void Tick(float DeltaTime) override;
+
+public:
+	UPROPERTY(VisibleDefaultsOnly)
+	UPhysicsHandleComponent* PhysicsHandleComponent;
+
+	UPROPERTY(EditAnywhere)
+	float GrabMaxDistance = 1000.f;
+
+	UPROPERTY(EditAnywhere)
+	float GrabMinDistance = 34.f;
+
+	UPROPERTY(EditAnywhere)
+	float ZoomSpeed = 10.f;
+
+	virtual void OnFire_Implementation() override;
+	virtual void OnZoom_Implementation(float Value) override;
+
+	UFUNCTION()
+	bool Grab();
+
+	UFUNCTION()
+	void Release();
+
+private:
+	bool bGrabActive;
+	UPrimitiveComponent* GrabbedComponent;
+	float GrabDistance;
+	FVector GrabLocation;
+};
