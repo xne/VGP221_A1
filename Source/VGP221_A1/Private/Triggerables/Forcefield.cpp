c76bf50ec5618d28ece350b2e7b3e95265d612d9
Erika <2530460+xne@users.noreply.github.com>
2025-11-29 01:06:05 -0500

    Add fade in/out animation to Forcefield actor

--- Source/VGP221_A1/Private/Triggerables/Forcefield.cpp
+++ Source/VGP221_A1/Private/Triggerables/Forcefield.cpp
@@ -2,6 +2,8 @@
 
 AForcefield::AForcefield()
 {
+	PrimaryActorTick.bCanEverTick = true;
+
 	if (!ForcefieldMeshComponent)
 	{
 		ForcefieldMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ForcefieldMeshComponent"));
@@ -16,16 +18,70 @@ AForcefield::AForcefield()
 	}
 }
 
+void AForcefield::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (ForcefieldMeshComponent)
+		ForcefieldMaterial = ForcefieldMeshComponent->CreateDynamicMaterialInstance(0);
+}
+
+void AForcefield::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	switch (FadeState)
+	{
+	case EFadeState::In:
+		LinearOpacity = FMath::FInterpConstantTo(LinearOpacity, 1.f, DeltaTime, 1.f / FadeTime);
+
+		if (FMath::IsNearlyEqual(LinearOpacity, 1.f, KINDA_SMALL_NUMBER))
+		{
+			LinearOpacity = 1.f;
+			FadeState = EFadeState::None;
+		}
+
+		break;
+	case EFadeState::Out:
+		LinearOpacity = FMath::FInterpConstantTo(LinearOpacity, 0.f, DeltaTime, 1.f / FadeTime);
+
+		if (FMath::IsNearlyZero(LinearOpacity, KINDA_SMALL_NUMBER))
+		{
+			LinearOpacity = 0.f;
+			FadeState = EFadeState::None;
+			SetActorEnableCollision(false);
+		}
+
+		break;
+	}
+
+	if (ForcefieldMaterial)
+	{
+		// Apply exponential interpolation to counteract the non-linear behavior of Fresnel
+		auto Opacity = FMath::InterpExpoIn(0.f, 1.f, LinearOpacity);
+		ForcefieldMaterial->SetScalarParameterValue(FName("Opacity"), Opacity);
+	}
+}
+
+void AForcefield::FadeIn()
+{
+	FadeState = EFadeState::In;
+	SetActorEnableCollision(true);
+}
+
+void AForcefield::FadeOut()
+{
+	FadeState = EFadeState::Out;
+}
+
 void AForcefield::OnTrigger_Implementation()
 {
-	if (IsHidden())
+	if (FadeState == EFadeState::Out || FadeState == EFadeState::None && FMath::IsNearlyZero(LinearOpacity, KINDA_SMALL_NUMBER))
 	{
-		SetActorHiddenInGame(false);
-		SetActorEnableCollision(true);
+		FadeIn();
 	}
 	else
 	{
-		SetActorHiddenInGame(true);
-		SetActorEnableCollision(false);
+		FadeOut();
 	}
 }
--- Source/VGP221_A1/Public/Triggerables/Forcefield.h
+++ Source/VGP221_A1/Public/Triggerables/Forcefield.h
@@ -4,6 +4,14 @@
 #include "Triggerables/FPSTriggerable.h"
 #include "Forcefield.generated.h"
 
+UENUM(BlueprintType)
+enum class EFadeState : uint8
+{
+	None,
+	In,
+	Out
+};
+
 UCLASS()
 class VGP221_A1_API AForcefield : public AFPSTriggerable
 {
@@ -12,8 +20,27 @@ class VGP221_A1_API AForcefield : public AFPSTriggerable
 public:
 	AForcefield();
 
+protected:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+public:
 	UPROPERTY(VisibleDefaultsOnly, Category = "Forcefield")
 	UStaticMeshComponent* ForcefieldMeshComponent;
 
+	UPROPERTY(EditAnywhere, Category = "Forcefield")
+	float FadeTime = 0.25f;
+
+	UFUNCTION(BlueprintCallable)
+	void FadeIn();
+
+	UFUNCTION(BlueprintCallable)
+	void FadeOut();
+
 	virtual void OnTrigger_Implementation() override;
+
+private:
+	UMaterialInstanceDynamic* ForcefieldMaterial;
+	EFadeState FadeState = EFadeState::None;
+	float LinearOpacity = 1.f;
 };
